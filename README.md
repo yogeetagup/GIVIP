0e8773293aae592330787ae6cbe29f9dc285b801
Yogeeta gupta <yogeetagup@gmail.com>
2019-01-30 22:31:24 +0530

    Added the code for the directory reading

--- README.md
+++ README.md
@@ -9,3 +9,11 @@ control status of the Repository
 `1.`  Run `./install.sh` to install the required dependencies.
 
 `2.`  Run `main.py`.
+
+## Notes to Consider
+
+1. If no directory is passed as the argument in main.py, 
+it will consider the current directory to run the code.
+
+2. provide the directory as the relative path from the current 
+directory
--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-def print_git_info():
-    print("Entered function")
-    print("Started execution")
+import os
+import sys
+
+REPO_DIR = '.git'
+CURR_DIR = '.'
+
+
+def print_git_info(dir_to_be_checked):
+    is_repo = False
+    for o in os.listdir(dir_to_be_checked):
+        if o == REPO_DIR:
+            is_repo = True
+        else:
+            pass
+    print('Current Directory is Source Controlled') if is_repo else print('Current Directory is not source controlled')
 
 
 if __name__ == '__main__':
-    print_git_info()
+    current_directory_path = ''
+    # import pdb
+    # pdb.set_trace()
+    if len(sys.argv) == 1:
+        current_directory_path = CURR_DIR
+    else:
+        current_directory_path = sys.argv[1]
+    print_git_info(current_directory_path)
